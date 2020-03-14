f3c4808e4b760ee57fa135b2cb36425944dd0781
Jonathan Winter <jonathan_winter@hotmail.com>
2020-03-14 20:19:27 +0100

    Add project files.

---
+++ StairBot.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StairBot.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
