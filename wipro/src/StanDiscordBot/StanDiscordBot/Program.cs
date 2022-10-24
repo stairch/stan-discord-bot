7450bc2015d3aaf317a1b97b5688b50d97edfa6d
Nicolas Wiedmer <nicolas.wiedmer999@gmail.com>
2022-10-24 18:14:37 +0200

    Start Discord Client and enable logging

--- wipro/src/StanDiscordBot/StanDiscordBot/Program.cs
+++ wipro/src/StanDiscordBot/StanDiscordBot/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StanBot.Services;
+
+namespace StanBot
+{
+    public class Program
+    {
+
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _discordSocketClient;
+        private IConfiguration _configuration;
+
+        public async Task MainAsync()
+        {
+
+            _discordSocketClient = new DiscordSocketClient();
+            _configuration = BuildConfiguration();
+
+            var services = ConfigureServices();
+            services.GetRequiredService<LogService>();
+
+            await _discordSocketClient.LoginAsync(TokenType.Bot, _configuration["DiscordApplicationToken"]);
+            await _discordSocketClient.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private IServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton(_discordSocketClient)
+                // add service
+                // add logging
+                .AddLogging()
+                .AddSingleton<LogService>()
+                // extra
+                .AddSingleton(_configuration)
+                .BuildServiceProvider();
+        }
+
+        private IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile($"stan.json")
+                .Build();
+        }
+    }
+}
