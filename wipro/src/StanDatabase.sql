3313c16fbe5a604cc0cdfdd20054a5f55eca95d7
Yannis Kraemer <yannkra21@gmail.com>
2022-10-28 10:33:14 +0200

    Fix errors in StanDatabase.sql.

--- wipro/src/StanDatabase.sql
+++ wipro/src/StanDatabase.sql
@@ -3,47 +3,47 @@ USE StanDB;
 
 -- https://www.w3schools.com/mysql/mysql_datatypes.asp
 CREATE TABLE DiscordCategories (
-    DiscordCategoryId INT NOT NULL,
+    DiscordCategoryId INT AUTO_INCREMENT NOT NULL,
     DiscordCategoryName VARCHAR(255) NOT NULL,
     PRIMARY KEY (DiscordCategoryId)
 );
 
 CREATE TABLE DiscordRoles (
-    DiscordRoleId INT NOT NULL,
+    DiscordRoleId INT AUTO_INCREMENT NOT NULL,
     DiscordRoleName VARCHAR(255) NOT NULL,
     PRIMARY KEY (DiscordRoleId)
 );
 
 CREATE TABLE Houses (
-    HouseId INT NOT NULL,
+    HouseId INT AUTO_INCREMENT NOT NULL,
     HouseName VARCHAR(255) NOT NULL,
     FkDiscordRoleId INT,
     PRIMARY KEY (HouseId),
-    CONSTRAINT FkDiscordRoleId FOREIGN KEY (DiscordRoleId) REFERENCES DiscordRole(DiscordRoleId)
+    FOREIGN KEY (FkDiscordRoleId) REFERENCES DiscordRoles(DiscordRoleId)
 );
 
 CREATE TABLE Modules (
-    ModuleId INT NOT NULL,
+    ModuleId INT AUTO_INCREMENT NOT NULL,
     ChannelName VARCHAR(255) NOT NULL,
     FullModuleName VARCHAR(255),
-    FkCategoryId INT,
+    FkDiscordCategoryId INT,
     PRIMARY KEY (ModuleId),
-    CONSTRAINT FkDiscordCategoryId FOREIGN KEY (DiscordCategoryId) REFERENCES DiscordCategories(DiscordCategoryId)
+    FOREIGN KEY (FkDiscordCategoryId) REFERENCES DiscordCategories(DiscordCategoryId)
 );
 
 CREATE TABLE Students (
-    StudentId INT NOT NULL,
+    StudentId INT AUTO_INCREMENT NOT NULL,
     StudentEmail VARCHAR(255) NOT NULL,
     StillStudying BOOL NOT NULL,
     SEMESTER TINYINT NOT NULL,
     FkHouseId INT NOT NULL,
     IsDiscordAdmin BOOL NOT NULL,
     PRIMARY KEY (StudentId),
-    CONSTRAINT FkHouseId FOREIGN KEY (HouseId) REFERENCES Houses(HouseId)
+    FOREIGN KEY (FkHouseId) REFERENCES Houses(HouseId)
 );
 
 CREATE TABLE DiscordAccounts (
-    DiscordAccountId INT NOT NULL,
+    DiscordAccountId INT AUTO_INCREMENT NOT NULL,
     Username VARCHAR(255),
     AccountId INT,
     ActivationCode VARCHAR(255),
@@ -58,19 +58,17 @@ CREATE TABLE DiscordAccountsModules (
     FkModuleId INT,
      -- https://stackoverflow.com/questions/5835978/how-to-properly-create-composite-primary-keys-mysql
     PRIMARY KEY (FkDiscordAccountId, FkModuleId),
-    CONSTRAINT FkDiscordAccountId FOREIGN KEY (DiscordAccountId) REFERENCES DiscordAccounts(DiscordAccountId),
-    CONSTRAINT FkModuleId FOREIGN KEY (ModuleId) REFERENCES Modules(ModuleId)
+    FOREIGN KEY (FkDiscordAccountId) REFERENCES DiscordAccounts(DiscordAccountId),
+    FOREIGN KEY (FkModuleId) REFERENCES Modules(ModuleId)
 );
 
 CREATE TABLE DiscordAccountsDiscordRoles (
     FkDiscordAccountId INT NOT NULL,
     FkDiscordRoleId INT NOT NULL,
-    PRIMARY KEY (FkDiscordAccountId, FkDiscordRoleId),
-    CONSTRAINT FkDiscordAccountId FOREIGN KEY (DiscordAccountId) REFERENCES DiscordAccounts(DiscordAccountId),
-    CONSTRAINT FkDiscordRoleId FOREIGN KEY (DiscordRoleId) REFERENCES DiscordRoles(DiscordRoleId)
+    #--PRIMARY KEY (FkDiscordAccountId, FkDiscordRoleId),
+    FOREIGN KEY (FkDiscordAccountId) REFERENCES DiscordAccounts(DiscordAccountId),
+    FOREIGN KEY (FkDiscordRoleId) REFERENCES DiscordRoles(DiscordRoleId)
 );
-
--- CONSTRAINT  FOREIGN KEY () REFERENCES (),
 
 -- Add houses
 INSERT INTO DiscordRoles (DiscordRoleName)
@@ -84,9 +82,9 @@ VALUES
 
 INSERT INTO Houses (HouseName, FkDiscordRoleId)
 VALUES
-    ("GREY", SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HouseGrey"),
-    ("Yellow", SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HouseYellow"),
-    ("BLUE", SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HouseBlue"),
-    ("PURPLE", SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HousePurple"),
-    ("ORANGE", SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HouseOrange"),
-    ("RED", SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HouseRed");
+    ("GREY", (SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HouseGrey")),
+    ("Yellow", (SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HouseYellow")),
+    ("BLUE", (SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HouseBlue")),
+    ("PURPLE", (SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HousePurple")),
+    ("ORANGE", (SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HouseOrange")),
+    ("RED", (SELECT DiscordRoleId FROM DiscordRoles WHERE DiscordRoleName = "HouseRed"));
