cbe2dcbd49c635e98d0e29ca167e16ed2b10b06e
Yannis Kraemer <yannkra21@gmail.com>
2022-11-15 21:35:26 +0100

    Add UnitTests for ModuleUtil.

--- wipro/src/StanDiscordBot/StanDatabase/Util/ModuleUtil.cs
+++ wipro/src/StanDiscordBot/StanDatabase/Util/ModuleUtil.cs
@@ -13,9 +13,9 @@ namespace StanDatabase.Util
 
         /// <summary>
         /// occasion number = Anlassnummer
-        // examples:
-        // I.BA_MAT1.17
-        // I.BA_NETW2_MM.21
+        /// examples:
+        /// I.BA_MAT1.17
+        /// I.BA_NETW2_MM.21
         /// </summary>
         /// <param name="occasionNumber"></param>
         /// <returns></returns>
@@ -28,28 +28,25 @@ namespace StanDatabase.Util
                 int indexOfIsaInName = Array.IndexOf(subName, "ISA");
                 if (indexOfIsaInName == subName.Length - 1)
                 {
-                    shortName = subName.ElementAt(indexOfIsaInName - 1) + '_' + subName.ElementAt(indexOfIsaInName);
+                    shortName = subName.ElementAt(indexOfIsaInName - 1);
                 }
                 else
                 {
-                    shortName = subName.ElementAt(indexOfIsaInName) + '_' + subName.ElementAt(indexOfIsaInName + 1);
+                    shortName = subName.ElementAt(indexOfIsaInName + 1);
                 }
                 return shortName;
             }
             else
             {
-                // TODO: maybe use full name as a help? Cut out string that differentiates in short name when the full name is the same
-                string[] subName = occasionNumber.Split('.')[1].Split('_');
+                List<string> subName = occasionNumber.Split('.')[1].Split('_').ToList();
+                subName.Remove("EVA");
+                subName.Remove("ISA");
                 // source: https://stackoverflow.com/questions/7461080/fastest-way-to-check-if-string-contains-only-digits-in-c-sharp
-                if (subName.Length >= 3 && !subName[1].All(char.IsDigit) && !subName[2].All(char.IsDigit))
-                {
-                    return subName[1] + "_" + subName[2];
-                }
-                else if (!subName[1].All(char.IsDigit))
+                if (!subName[1].All(char.IsDigit))
                 {
                     return subName[1];
                 }
-                else // if (!subName[2].All(char.IsDigit))
+                else
                 {
                     return subName[2];
                 }
