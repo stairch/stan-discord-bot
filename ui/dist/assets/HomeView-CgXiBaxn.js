cf0d3debc19462cc784608b2a8dc9453c759aec1
David Dias Horta <david.diashorta@stud.hslu.ch>
2024-06-04 16:04:05 +0200

    ui: deployment

--- ui/dist/assets/HomeView-CgXiBaxn.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t,a as c}from"./index-BnolgutE.js";const n={},s=c("h1",null,"PR Toolkit",-1),a=[s];function _(r,l){return o(),t("main",null,a)}const d=e(n,[["render",_]]);export{d as default};
