1b692fa8232d98e3951fcc829b1db6acd2ee79dd
David Dias Horta <david.diashorta@stud.hslu.ch>
2024-09-22 17:29:22 +0200

    icon font: add 'schedule' icon

--- ui/dist/assets/cssMode-CpyGWpqc.js
+++ ui/dist/assets/cssMode-CpyGWpqc.js
@@ -1,4 +1,4 @@
-import{m as et}from"./index-AkriktXb.js";import"./index-BQcjMRzu.js";import"./announcements-colqhpwp.js";import"./LoadingWithResultModal-BzhTeCZB.js";/*!-----------------------------------------------------------------------------
+import{m as et}from"./index-NS386CfS.js";import"./index-j93gbVpD.js";import"./announcements-CFz7yyhb.js";import"./LoadingWithResultModal-BORwS2x2.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
--- ui/src/assets/icons/font/icons.ts
+++ ui/src/assets/icons/font/icons.ts
@@ -2,18 +2,21 @@ export type IconsId =
   | "discord"
   | "edit"
   | "instagram"
+  | "schedule"
   | "telegram";
 
 export type IconsKey =
   | "Discord"
   | "Edit"
   | "Instagram"
+  | "Schedule"
   | "Telegram";
 
 export enum Icons {
   Discord = "discord",
   Edit = "edit",
   Instagram = "instagram",
+  Schedule = "schedule",
   Telegram = "telegram",
 }
 
@@ -21,5 +24,6 @@ export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
   [Icons.Discord]: "61697",
   [Icons.Edit]: "61698",
   [Icons.Instagram]: "61699",
-  [Icons.Telegram]: "61700",
+  [Icons.Schedule]: "61700",
+  [Icons.Telegram]: "61701",
 };
