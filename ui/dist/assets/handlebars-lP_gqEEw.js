d61ff99855243508ace5b62f3f56148eaaeec5bf
David Dias Horta <david.diashorta@stud.hslu.ch>
2024-09-24 19:29:21 +0200

    ui: build update

--- ui/dist/assets/handlebars-lP_gqEEw.js
+++ ui/dist/assets/handlebars-lP_gqEEw.js
@@ -1,4 +1,4 @@
-import{m as i}from"./index-BM7DYJnz.js";import"./index-uMy-efuz.js";import"./announcements-CLws6go6.js";import"./LoadingWithResultModal-g8QimMQR.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./index-1L3QHknZ.js";import"./index-CnnRNlq9.js";import"./announcements-BPhsDQ27.js";import"./LoadingWithResultModal-Dtyhi4x9.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
--- ui/dist/assets/javascript-D3hxUB1M.js
+++ ui/dist/assets/javascript-D3hxUB1M.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript--PpRNMhf.js";import"./index-BM7DYJnz.js";import"./index-uMy-efuz.js";import"./announcements-CLws6go6.js";import"./LoadingWithResultModal-g8QimMQR.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-B4xxqAy1.js";import"./index-1L3QHknZ.js";import"./index-CnnRNlq9.js";import"./announcements-BPhsDQ27.js";import"./LoadingWithResultModal-Dtyhi4x9.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
