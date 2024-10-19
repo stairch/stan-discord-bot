b1c53e9d0d7be1be7942b8e78d5b205e6aeb21b0
David Dias Horta <david.diashorta@stud.hslu.ch>
2024-10-19 14:04:04 +0200

    ui: build update

--- ui/dist/assets/javascript-BJUBeqa1.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript-grR8gqqH.js";import"./index-PP2DCH2i.js";import"./index-Ctoyu7k7.js";import"./announcements-C8FuxwCo.js";import"./LoadingWithResultModal-CU-xc4Os.js";import"./___vite-browser-external_commonjs-proxy-D5Dao81r.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
